29c7621f0a33cd4a0d07de02fbc337b0c89ba13d
Sourav Pal <palsusanta1968@gmail.com>
2025-03-21 10:15:46 +0530

    Add navigation link to home in navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import AuthButton from './AuthButton'
 import {
     SignedIn,
@@ -9,10 +10,13 @@ import {
 const Navbar = () => {
     return (
         <nav className="w-full flex justify-between items-center px-6 sm:px-10 py-6 absolute top-0 z-10">
-            <h1 className="text-white text-2xl md:text-3xl font-Warnes!">
-                BuzzIQ
-            </h1>
-            <div className="">
+            <Link to="/">
+                <h1 className="text-white text-2xl md:text-3xl font-Warnes! cursor-pointer">
+                    BuzzIQ
+                </h1>
+            </Link>
+
+            <div>
                 <SignedOut>
                     <AuthButton />
                 </SignedOut>
