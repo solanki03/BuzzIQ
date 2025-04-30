06a75b9dd6f2accb9e59dfaee1ef4283e51226fa
solanki03 <solankisingha23@gmail.com>
2025-04-30 15:25:40 +0530

    -- fix updated

--- server/index.js
+++ server/index.js
@@ -16,6 +16,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
