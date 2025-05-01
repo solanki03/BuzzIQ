4e78771845dfc84f70660f89c5b4c04a7f755cb3
DevNasim01 <sknasimpc@gmail.com>
2025-05-02 00:08:11 +0530

    -- fix

--- server/app.js
+++ server/app.js
@@ -8,11 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: '*',
-  methods: ['GET','POST'],
-  credentials: true,
-}));
+app.use(cors());
 
 // Results database connection
 // Replace your resultsDB connection with this:
