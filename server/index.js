baf408596e0223a3cf028102f3f5dba705e1be28
DevNasim01 <sknasimpc@gmail.com>
2025-05-02 00:28:04 +0530

    -- fix

--- server/index.js
+++ server/index.js
@@ -1,16 +1,15 @@
-// index.js
+// index.js - Simplified
 const dotenv = require('dotenv');
 dotenv.config();
 
 const connectDB = require('./config/db');
-const app = require('./app');  // <-- We’ll define routes in app.js
+const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
-// Connect to the database
-connectDB();
-
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// Connect to DB and start server
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
