ce084fb5f0276e207c2897099cdc52bd10dbaa47
DevNasim01 <sknasimpc@gmail.com>
2025-05-01 22:43:31 +0530

    -- fix

--- server/app.js
+++ server/app.js
@@ -8,11 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: '*',
-  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
-  credentials: true,
-}));
+app.use(cors());
 
 // Results database connection
 const resultsDB = mongoose.createConnection(process.env.MONGODB_URI, {
