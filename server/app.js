1c2bd5842b762fa8a5198620c49fbf9c5712e370
Sourav Pal <palsusanta1968@gmail.com>
2025-05-02 00:16:09 +0530

    fix

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'https://buzz-iq.vercel.app/',
+  origin: '*',
   methods: ['GET','POST'],
   credentials: true,
 }));
